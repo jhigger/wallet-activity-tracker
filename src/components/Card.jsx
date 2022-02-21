7771c547712f3aaac82c11f08a282146104a08fe
jhigger <njohnjhigger@gmail.com>
2022-02-21 17:50:38 +0800

    link to explorer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,11 @@ import {
 	useColorModeValue,
 	Image,
 	Spacer,
-	Text
+	Text,
+	VStack,
+	IconButton
 } from '@chakra-ui/react';
+import {BsBoxArrowUpRight} from 'react-icons/bs';
 
 const NFT = ({src}) => {
 	return (
@@ -24,61 +27,75 @@ const NFT = ({src}) => {
 	);
 };
 
-const Details = ({name, marketplace, amount, type, time}) => {
+const Details = ({name, marketplace, amount, type, time, url}) => {
 	return (
-		<Flex p={2} w="full" flexDirection={{base: 'column', md: 'row'}}>
-			<Box p="2">
-				<Text
-					fontSize={{base: '2xl', lg: '3xl'}}
-					color={useColorModeValue('gray.800', 'white')}
-					fontWeight="bold"
-					maxWidth={{base: '48', lg: 'sm'}}
-					isTruncated
-				>
-					{name}
-				</Text>
-				<Box mt={2}>
-					<chakra.span
-						color={useColorModeValue('gray.600', 'gray.400')}
-						display={{base: 'none', lg: 'inline'}}
-					>
-						Marketplace:{' '}
-					</chakra.span>
+		<VStack w="full">
+			<Flex p={2} w="full" flexDirection={{base: 'column', md: 'row'}}>
+				<Box p="2">
 					<Text
-						display={{base: 'inline-block', lg: 'inline'}}
+						fontSize={{base: '2xl', lg: '3xl'}}
 						color={useColorModeValue('gray.800', 'white')}
-						maxWidth={{base: '48', lg: 'xs'}}
+						fontWeight="bold"
+						maxWidth={{base: '48', lg: 'sm'}}
 						isTruncated
 					>
-						{marketplace}
+						{name}
+					</Text>
+					<Box mt={2}>
+						<chakra.span
+							color={useColorModeValue('gray.600', 'gray.400')}
+							display={{base: 'none', lg: 'inline'}}
+						>
+							Marketplace:{' '}
+						</chakra.span>
+						<Text
+							display={{base: 'inline-block', lg: 'inline'}}
+							color={useColorModeValue('gray.800', 'white')}
+							maxWidth={{base: '48', lg: 'xs'}}
+							isTruncated
+						>
+							{marketplace}
+						</Text>
+					</Box>
+					<Box mt={2}>
+						<chakra.span
+							color={useColorModeValue('brand.600', 'brand.300')}
+							display={{base: 'none', lg: 'inline'}}
+						>
+							Amount:{' '}
+						</chakra.span>
+						<chakra.span color={useColorModeValue('gray.800', 'white')}>
+							{amount} SOL
+						</chakra.span>
+					</Box>
+				</Box>
+				<Spacer />
+				<Box minW="min" p="2" textAlign={{base: 'start', md: 'end'}}>
+					<chakra.h2
+						fontSize={{base: '2xl', lg: '3xl'}}
+						color={type == 'BOUGHT' ? 'green.300' : 'red.300'}
+						fontWeight="bold"
+					>
+						{type}
+					</chakra.h2>
+					<Text mt={2} color={useColorModeValue('gray.600', 'gray.400')}>
+						{time}
 					</Text>
 				</Box>
-				<Box mt={2}>
-					<chakra.span
-						color={useColorModeValue('brand.600', 'brand.300')}
-						display={{base: 'none', lg: 'inline'}}
-					>
-						Amount:{' '}
-					</chakra.span>
-					<chakra.span color={useColorModeValue('gray.800', 'white')}>
-						{amount} SOL
-					</chakra.span>
-				</Box>
-			</Box>
+			</Flex>
 			<Spacer />
-			<Box minW="min" p="2" textAlign={{base: 'start', md: 'end'}}>
-				<chakra.h2
-					fontSize={{base: '2xl', lg: '3xl'}}
-					color={type == 'BOUGHT' ? 'green.300' : 'red.300'}
-					fontWeight="bold"
-				>
-					{type}
-				</chakra.h2>
-				<Text mt={2} color={useColorModeValue('gray.600', 'gray.400')}>
-					{time}
-				</Text>
+			<Box p="2" textAlign={{base: 'start', md: 'end'}} alignSelf="end">
+				<IconButton
+					size="md"
+					fontSize="md"
+					aria-label={'View in explorer'}
+					variant="ghost"
+					color="current"
+					onClick={() => window.open(url, '_blank').focus()}
+					icon={<BsBoxArrowUpRight />}
+				/>
 			</Box>
-		</Flex>
+		</VStack>
 	);
 };
 
@@ -89,6 +106,7 @@ export default function Card({
 	total_amount,
 	transaction_type,
 	time,
+	url,
 	...props
 }) {
 	return (
@@ -109,6 +127,7 @@ export default function Card({
 				amount={total_amount}
 				type={transaction_type.toUpperCase()}
 				time={time}
+				url={url}
 			/>
 		</Box>
 	);
